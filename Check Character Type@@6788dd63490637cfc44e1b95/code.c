fa3baa450175c95a7c2e4a3918392c83d6469bbd
MunishGarg1234 <munish1380.becse24@chitkara.edu.in>
2025-01-31 15:45:12 +0530

    Solution to Check Character Type

--- Check Character Type@@6788dd63490637cfc44e1b95/code.c
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -2,19 +2,19 @@
 int main() {
     char ch;
     scanf("%c",ch);
-    if((ch>='a' && ch<='z')||(ch>='A' && ch<='Z')){
-        if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U'){
-            printf("Vowel");
-        }
-        else{
-            printf("Consonant");
-        }
+    // if((ch>='a' && ch<='z')||(ch>='A' && ch<='Z')){
+    //     if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U'){
+    //         printf("Vowel");
+    //     }
+    //     else{
+    //         printf("Consonant");
+    //     }
+    // }
+    if(ch>='0' && ch<='9'){
+        printf("Digit");
     }
-    // else if(ch>='0' && ch<='9'){
-    //     printf("Digit");
-    // }
-    // else{
-    //     printf("Special Character");
-    // }
+    else{
+        printf("Special Character");
+    }
     return 0;
 }
