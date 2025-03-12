c361ce42653513ad8f1517fa76e7753dff2f7dab
MunishGarg1234 <munish1380.becse24@chitkara.edu.in>
2025-03-12 15:24:56 +0530

    Solution to Count Vowels in a String

--- Count Vowels in a String@@676c039d80e1714600f67641/code.c
+++ Count Vowels in a String@@676c039d80e1714600f67641/code.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+#include<ctype.h>
 int main() {
     char ch[50];
     fgets(ch,50,stdin);
     int count=0;
     for(int i=0;ch[i]!=0;i++){
-        if(ch[i]=='a'|| ch[i]=='e'||ch[i]=='i'||ch[i]=='o'||ch[i]=='u'||ch[i]=='A'||ch[i]=='E'||ch[i]=='I'||ch[i]=='O'||ch[i]=='U'){
+        ch[i]=tolower(ch[i]);
+        if(ch[i]=='a'|| ch[i]=='e'||ch[i]=='i'||ch[i]=='o'||ch[i]=='u'){
             count++;
         }
     }
