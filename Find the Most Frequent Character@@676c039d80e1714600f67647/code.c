26eb28dc634b13d31c0678d465dacfecd986b432
MunishGarg1234 <munish1380.becse24@chitkara.edu.in>
2025-03-19 18:52:52 +0530

    Solution to Find the Most Frequent Character

--- Find the Most Frequent Character@@676c039d80e1714600f67647/code.c
+++ Find the Most Frequent Character@@676c039d80e1714600f67647/code.c
@@ -1,33 +1,22 @@
 #include <stdio.h>
 #include <string.h>
-
-#define MAX_CHAR 256  // Total ASCII characters
-
+#define MAX_CHAR 256
 int main() {
     char str[100], result;
     int freq[MAX_CHAR] = {0}, maxFreq = 0;
-
-    // Input string
-    printf("Enter a string: ");
     fgets(str, sizeof(str), stdin);
-
-    // Count frequency of each character
     for (int i = 0; str[i] != '\0'; i++) {
-        if (str[i] != '\n') { // Ignore newline from fgets
+        if (str[i] != '\n') {
             freq[(int)str[i]]++;
         }
     }
-
-    // Find the most frequent character (alphabetically first in case of tie)
     for (int i = 0; i < MAX_CHAR; i++) {
         if (freq[i] > maxFreq) {
             maxFreq = freq[i];
             result = (char)i;
         }
     }
-
-    // Output result
-    printf("Most frequent character: '%c' -> %d times\n", result, maxFreq);
+    printf(" '%c' %d", result, maxFreq);
 
     return 0;
 }
