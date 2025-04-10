697a17adec9583db7522da3b0618ada0a3eea6f8
MunishGarg1234 <munish1380.becse24@chitkara.edu.in>
2025-04-10 18:14:41 +0530

    Solution to Lost & Found – Matching Gloves

--- Lost & Found – Matching Gloves@@67de00956ac3fec9afb90e19/code.c
+++ Lost & Found – Matching Gloves@@67de00956ac3fec9afb90e19/code.c
@@ -6,9 +6,12 @@ int countGlovePairs(int gloves[],int n){
     }
     int count1=0;
     for(int i=0;i<99;i++){
-        if(count[i]%2==0 && count[i]!=0){
+        if(count[i]!=0){
             count1=count1+count[i]/2;
         }
+        // else if(count[i]%2!=0){
+        //     count1=count1+count[i]%2;
+        // }
     }
     return count1;
 }
