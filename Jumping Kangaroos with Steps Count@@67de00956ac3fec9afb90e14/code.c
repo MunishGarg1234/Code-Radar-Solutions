db13b1c0b38d6f89ad652a53b660ee41eb9dc164
MunishGarg1234 <munish1380.becse24@chitkara.edu.in>
2025-04-10 17:39:58 +0530

    Solution to Jumping Kangaroos with Steps Count

--- Jumping Kangaroos with Steps Count@@67de00956ac3fec9afb90e14/code.c
+++ Jumping Kangaroos with Steps Count@@67de00956ac3fec9afb90e14/code.c
@@ -1,13 +1,16 @@
 // Your code here...
 int kangaroo(int x1,int v1,int x2,int v2){
     int a=1;
-    // for(int i=0;i<=10;i++){
-    //     if(x1+i*v1 == x2+i*v2){
-    //         return i;
-    //         a=0;
-    //         break;
-    //     }
-    // }
+    int x,y;
+    for(int i=0;i<=10;i++){
+        x=x1+i*v1;
+        y=x2+i*v2;
+        if(x==y){
+            return i;
+            a=0;
+            break;
+        }
+    }
     if(a==1){
         return -1;
     }
