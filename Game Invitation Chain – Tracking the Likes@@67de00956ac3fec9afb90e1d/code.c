e94a14e6eef08201aae4c1531eeac13663c680bd
MunishGarg1234 <munish1380.becse24@chitkara.edu.in>
2025-04-10 14:06:23 +0530

    Solution to Game Invitation Chain – Tracking the Likes

--- Game Invitation Chain – Tracking the Likes@@67de00956ac3fec9afb90e1d/code.c
+++ Game Invitation Chain – Tracking the Likes@@67de00956ac3fec9afb90e1d/code.c
@@ -3,7 +3,13 @@ int totalLikes(int days){
     int likes=0,peoples=5;
     for(int i=1;i<=days;i++){
         likes=likes+(peoples/2);
-        peoples+=3;
+        if(days=1){
+            peoples+=3;
+        }
+        else{
+            peoples=peoples-2;
+        }
+        
     }
     return likes;
 }
