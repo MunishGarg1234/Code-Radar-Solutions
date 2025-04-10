594f4b80f45381a136e19a42b8a3e04e3c3d9697
MunishGarg1234 <munish1380.becse24@chitkara.edu.in>
2025-04-10 17:09:41 +0530

    Solution to Gaming Tournament – Most Played Game

--- Gaming Tournament – Most Played Game@@67de00956ac3fec9afb90e18/code.c
+++ Gaming Tournament – Most Played Game@@67de00956ac3fec9afb90e18/code.c
@@ -2,7 +2,7 @@
 int mostPlayedGame(int games[],int n){
     int arr1[10]={0};
     for(int i=0;i<n;i++){
-        arr1[arr[0]]++;
+        arr1[games[0]]++;
     }
     int a=0;
     for(int i=0;i<10;i++){
